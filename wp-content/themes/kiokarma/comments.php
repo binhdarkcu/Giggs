907d40763cca91452d50f10e01f127df23f641da
[lamthanhbinh] <[binh@bliss-interactive.net]>
2015-01-15 17:31:08 +0700

    '

--- wp-content/themes/kiokarma/comments.php
+++ wp-content/themes/kiokarma/comments.php
@@ -61,27 +61,30 @@ if ( post_password_required() )
 		   $GLOBALS['comment'] = $comment; ?>
 		
 		   <li <?php comment_class(); ?> id="li-comment-<?php comment_ID() ?>">
-		     <div id="comment-<?php comment_ID(); ?>" <?php if($isByAuthor){ echo 'class="author the-comment clearfix"';}?>>
-		      <div class="comment-header">
-		         <?php echo get_avatar( $comment->comment_author_email, 52 ); ?>
-		         <?php edit_comment_link(__('(Edit)'),'  ','') ?>
-		      </div>
-		      <div class="comment-core">
-		      	<div class="comment-author">
-					<strong><?php printf(__('%s'), get_comment_author()) ?></strong> said on June 9, 2014 at 5:06 pm: 
-					<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>				
-				</div><!-- end .comment-author .meta -->
-		     	 <div class="comment-text">
-			      	<?php comment_text() ?>
-				  </div>
-		      </div>
-		      <?php if ($comment->comment_approved == '0') : ?>
-		         <em><?php _e('Your comment is awaiting moderation.') ?></em>
-		         <br />
-		      <?php endif; ?>
-			 
-		      
-		     </div>
+		   	 <article id="div-comment-<?php comment_ID(); ?>" class="comment-body"> 
+		   	 	<footer class="comment-meta">
+		   	 		<div class="comment-author vcard"> 
+		   	 			<?php echo get_avatar( $comment->comment_author_email, 42 ); ?> 
+		   	 			<b class="fn"><?php printf(__('%s'), get_comment_author()) ?></b> 
+		   	 			<span class="says">says:</span>
+		   	 		</div>
+		   	 		<div class="comment-metadata"> 
+		   	 			<a href="http://demo.themerain.com/skills/meet-skills-a-focused-grid-theme/#comment-6"> 
+		   	 				<time datetime="<?php comment_date('n-j-Y'); ?>"> <?php comment_date('n-j-Y'); ?></time> 
+		   	 			</a>
+		   	 		</div>
+		   	 	</footer>
+		   	 	<div class="comment-content">
+		   	 		<p><?php comment_text() ?></p>
+		   	 	</div>
+		   	 	<div class="reply">
+		   	 		<?php comment_reply_link(array_merge( $args, array('depth' => $depth, 'max_depth' => $args['max_depth']))) ?>
+		   	 	</div> 
+		   	 </article>
+		   	
+		   	
+		   	
+		     
 		    </li>
 		<?php
 		}
